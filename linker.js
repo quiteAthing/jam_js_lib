324af648080e87e98a54f55b8425e7bcc7d5f13e
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-03-06 20:52:01 +0800

    go home.

---
+++ linker.js
@@ -0,0 +1,55 @@
+
+
+//登入畫面
+const agree_to_term="#accept-terms";//取得
+const btn_login="#login-submit";//一般登入
+const btn_logout="#logOut";//登出(不區分))
+const btn_continueFB="#login-fb";//FB登入
+const btn_register="#reg-submit";//註冊按鈕
+const field_ACC="#signup-email";//帳號欄
+const fieldPW="#signup-password";//密碼欄
+const fieldCPW="#signup-password-confirm";//確認密碼
+const fieldRemember_me="#remember-me";//記住我
+
+
+//會員資料編輯畫面
+const btn_uData_submit="#edit-submit";
+const field_pic="#member-pic";
+const field_instrument="#member-instruments";//數列：X,2,3,4,5;
+const field_nickname="#member-name";
+const field_uIntro="#member-edit-textarea";
+const field_links="#member-media";//數列：X,2,3,4,5,6;
+
+
+//站內信系統相關資料
+const btn_send_message="#send-message"//發送站內信
+const btn_cancel_message="";//取消發送
+const field_mailto="";
+const field_msgbody="";
+const cls_msgbody="nxx_msg";//訊息標籤最外層以此標示
+
+
+
+//網站狀態
+var sta=0;
+const not_initialized=0;
+const sta_notLoggedIn=1;
+const sta_loggedIn=2;
+//const loginURL="";
+//const loginFbURL="";
+//cookie 所屬的網域名稱，測試用
+//const base_path="";
+const c_loginType="loginType";
+const c_lt_fb="FB";
+const c_lt_normal="normal";
+const jam_cookie_key="jamInfo";
+const base_url="http://localhost:8080/jamTest";
+const service_login="/login";
+const service_checkACC="/checkAcc";
+const service_register="/register";
+const msg_length_limit=3000;
+const service_messsagebox="";
+const show_userName="";
+const show_login="";
+
+
--- message_system.js
+++ message_system.js
@@ -2,14 +2,115 @@
 //不使用jquery
 
 
-var msg=new Object();
 
-//需要傳入一個callback function，同時會對回乎函式傳入資料(訊息數量等)
-msg.checkNewMessage=function(callback){}
+/*
+linker element:
+	service_messagebox: chkMessage
+	msg_limit :訊息最大長度，整數
 
-//回傳message的內容，固定為一維陣列
-msg.getMessage=function(){}
 
-msg.sendMessage=function(){}
+標示用css類別：
+	nxx_msgbody : .....	
+*/
+
+
+
+
+var msg=makeMessage();
+	
+	
+	function(){
+		var methods={
+			checkNewMessage : checkNewMessage,//檢查是否有新訊息
+			getMessage :　getMessage,//取得完整訊息
+			sendMessage : sendMessage,//發送訊息給指定使用者
+			setupTest : setupTesting,//設定測試用環境，測試現場實作
+			chkMsgBody : checkMsgBody,//發送訊息前檢查，檢查訊息本文是否符合規定
+			checkMailto :chekMailto ,//發送訊息前檢查或當下，檢查寄件對象是否存在
+			showOnCheck :showOnCheck, //檢查完成後改變顯示內容
+			showOnNewMessage : showNewMessage,//如果有新訊息則將訊息顯示出來
+			checkMsgLength : checkMsgLength, //檢查長度設定
+			udata : null
+			
+		}
+		//檢查有沒有新信，呼叫showOnNewMessage
+		function chekcNewMessage(callback){
+			var req=new Object();
+			var xhr=new XMLHttpRequest();
+			xhr.onreadystatechange=function(resp){
+				
+				switch(xhr.readyState){
+					case 1:xhr.send();break;
+					case 4: if(xhr.status==200){
+							showOnNewMessage(JSON.parse(xhr.responseText));
+					}break;
+					default :console.log(xhr.status);break;
+				}
+			}
+			xhr.open("GET",base_url+service_messagebox,true);
+			
+			
+		}
+		
+		
+		//取得訊息本體，取得後呼叫showOnNewMessage將內容寫出
+		function getMessage(){
+			
+		}
+		
+		//發送訊息，info：物件，結構建下方
+		/*
+			info={
+				to_user:對象
+				msg:訊息本體
+			}
+		
+		*/
+		function sendMessage(info){
+			
+			
+		}
+		
+		
+		
+		function setupTesting(data){
+			//data{ 
+			//userid : xxxxxxx 
+			//}
+			//udata正常是要從cookie抓。
+			this.udata=data;
+			
+		}
+		
+		//檢查發文本文是否符合規定(字數、奇怪字元...etc)
+		//回傳數字陣列，表示狀況。1201:無問題 1:訊息超過長度 2:訊息空白 3:收件者格式錯誤
+		function checkMsgBody(callback){
+			
+			callback(1201);
+		}
+		//檢查寄件對象是否存在、格式是否正確
+		function checkMailto(){
+			
+		}
+		
+		//檢查訊息長度，並回傳長度(單位：字元數)
+		function checkMsgLength(){
+			var msgLng=0;
+			
+			return msgLng;
+		}
+		
+		function showOnNewMessage(){
+			
+		}
+		
+
+		
+		
+		
+		
+		
+		return methods
+	}
 
 
